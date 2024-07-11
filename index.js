32d752b6183b401eb4954420a52eaeb596cffff0
mark-s-bit <markchege03@gmail.com>
2024-07-11 15:30:50 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) 
+    return "This one is on me!";
+  else if (feet <= 2000 && feet >=400)
+    return "That will be twenty bucks.";
+  else if (feet >= 2000 && feet <=2500)
+    return "I will gladly take your thirty bucks.";
+  else(feet >=2500)
+    return "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(NYC){
+
+   return NYC === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(Tip){
+ // Write your code here!
+ switch(Tip){
+ case 'generous':
+  return 'Thank you so much.';
+  case 'not as generous':
+  return  'Thank you.';
+
+ default :
+ return 'Bye.';
+ }}
